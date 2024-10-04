11abfa38487b39da7a6e134232f594d37a3b57d3
Collins-rop <colinrop68@gmail.com>
2024-10-04 15:24:09 +0300

    (feat)business dev

--- src/main/java/com/bizna/biznakonnect/controller/HomeController.java
+++ src/main/java/com/bizna/biznakonnect/controller/HomeController.java
@@ -1,18 +1,30 @@
 package com.bizna.biznakonnect.controller;
 
+
+import com.bizna.biznakonnect.model.Business;
+import com.bizna.biznakonnect.service.BusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private BusinessService businessService;
+
+//    @GetMapping("/")
+//    public String home() {
+//        return "home";
+//    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Business> businesses = businessService.getAllBusinesses();
+        model.addAttribute("businesses", businesses);
         return "home";
     }
-
-//    @GetMapping("/register")
-//    public String register() {
-//        return "register";
-//    }
 }
